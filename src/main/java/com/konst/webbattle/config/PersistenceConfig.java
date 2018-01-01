27ebbf82854299c20830367be5f594e2ca2cdaeb
nick <konst007@tut.by>
2018-01-02 02:26:54 +0300

    add dependencies, change dataSource for heroku

--- src/main/java/com/konst/webbattle/config/PersistenceConfig.java
+++ src/main/java/com/konst/webbattle/config/PersistenceConfig.java
@@ -22,13 +22,15 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement(proxyTargetClass = true)
 @EnableJpaRepositories(basePackages = {"com.konst.webbattle"})
 public class PersistenceConfig {
-    @Bean
+    /*@Bean
     public DataSource dataSource() {
         BasicDataSource dataSource = new BasicDataSource();
         dataSource.setUrl("jdbc:mysql://localhost:3306/battle?useLegacyDatetimeCode=false&serverTimezone=UTC");
@@ -36,6 +38,22 @@ public class PersistenceConfig {
         dataSource.setUsername("root");
         dataSource.setPassword("123321");
         return dataSource;
+    }*/
+
+    @Bean
+    public DataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
     }
 
     @Bean
